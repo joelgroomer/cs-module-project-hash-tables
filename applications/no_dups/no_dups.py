48ce64916d603765223ba6598fb66cc7a9510121
Joel <joelgroomer@gmail.com>
2020-09-09 19:35:04 -0700

    no_dups complete

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,17 @@
 def no_dups(s):
     # Your code here
+    final = []
+    arr = s.split()
 
+    for word in arr:
+        if word not in final:
+            final.append(word)
+
+    str_final = ''
+    for word in final:
+        str_final += word + ' '
+
+    return str_final.strip()
 
 
 if __name__ == "__main__":
